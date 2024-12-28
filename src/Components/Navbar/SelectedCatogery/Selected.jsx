a9b985e3cd52ccc1bbafb1644837f5efef87b93e
Devansh <devanshjindal31@gmail.com>
2024-12-28 17:53:02 +0530

    404 error solution test 1

--- src/Components/Navbar/SelectedCatogery/Selected.jsx
+++ src/Components/Navbar/SelectedCatogery/Selected.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Selected({ select, datas, setTogled, setSelect }) {
   
@@ -17,7 +17,7 @@ function Selected({ select, datas, setTogled, setSelect }) {
     >
       <div className='p-5 flex flex-col gap-4 overflow-y-auto overflow-x-hidden max-h-[calc(100vh-8rem)] md:max-h-[calc(100vh-9rem)] mb-10 custom-scrollbar scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-white'>
         {datas?.map((data) => (
-          <NavLink to={data.link} onClick={handleToggle} key={data.title}>
+          <Link to={data.link} onClick={handleToggle} key={data.title}>
 
             <div className={`bg-white bg-opacity-70 py-4 px-6 flex items-center rounded-lg shadow-md transition-all duration-500 ease-in-out transform hover:scale-105 hover:bg-gradient-to-r hover:from-[#bbd1fe] hover:to-[#4567cd] hover:text-white`}>
               <div className='mr-3'>
@@ -25,7 +25,7 @@ function Selected({ select, datas, setTogled, setSelect }) {
               </div>
               <h1 className='text-black text-base md:text-lg'>{data.title}</h1>
             </div>
-          </NavLink>
+          </Link>
         ))}
       </div>
     </div>
